3062238c9184e16c1915e626248af41f42f6c3a2
Khoa Huynh <khoawin92@gmail.com>
2018-06-18 16:10:13 -0700

    fifo animal shelter completed

--- queues/__test__/fifo_animal_shelter.spec.js
+++ queues/__test__/fifo_animal_shelter.spec.js
@@ -31,5 +31,69 @@ describe('fifo animal shelter', () => {
     expect(shelter.head.next.name).toBe('kitty');
   });
 
+  it('dequeue should work with no preferences', () => {
+
+    let shelter = new AnimalShelter;
+
+    shelter.enqueue('doug', 'dog');
+    shelter.enqueue('kitty', 'cat');
+    shelter.enqueue('moo', 'cow');
+
+    expect(shelter.dequeue().name).toBe('doug');
+    expect(shelter.head.name).toBe('kitty');
+  });
+
+  it('dequeue should work with dog preference', () => {
+
+    let shelter = new AnimalShelter;
+
+    shelter.enqueue('kitty', 'cat');
+    shelter.enqueue('meow', 'cat');
+    shelter.enqueue('doug', 'dog');
+    shelter.enqueue('moo', 'cow');
+
+    expect(shelter.dequeue('dog').name).toBe('doug');
+    expect(shelter.head.name).toBe('kitty');
+  });
+
+  it('dequeue should work with cat preference', () => {
+
+    let shelter = new AnimalShelter;
+
+    shelter.enqueue('doug', 'dog');
+    shelter.enqueue('moo', 'cow');
+    shelter.enqueue('doggo', 'dog');
+    shelter.enqueue('kitty', 'cat');
+
+
+    expect(shelter.dequeue('cat').name).toBe('kitty');
+    expect(shelter.head.name).toBe('doug');
+  });
+
+  it('dequeue should work with dog preference when that dog is first in queue', () => {
+
+    let shelter = new AnimalShelter;
+
+    shelter.enqueue('doug', 'dog');
+    shelter.enqueue('moo', 'cow');
+    shelter.enqueue('kitty', 'cat');
+
+
+    expect(shelter.dequeue('dog').name).toBe('doug');
+    expect(shelter.head.name).toBe('kitty');
+  });
+
+  it('dequeue should work with cat preference when that cat is first in queue', () => {
+
+    let shelter = new AnimalShelter;
+
+    shelter.enqueue('kitty', 'cat');
+    shelter.enqueue('doug', 'dog');
+    shelter.enqueue('moo', 'cow');
+
+
+    expect(shelter.dequeue('cat').name).toBe('kitty');
+    expect(shelter.head.name).toBe('doug');
+  });
 
 });
--- queues/lib/fifo_animal_shelter.js
+++ queues/lib/fifo_animal_shelter.js
@@ -2,7 +2,6 @@
 
 class Animal {
   constructor(name, value) {
-
     this.name = name;
     this.type = value;
     this.next = null;
@@ -17,63 +16,77 @@ class AnimalShelter {
 
   enqueue(name, animal) {
 
-    let newAnimal = new Animal(name, animal);
+    if (animal === 'dog' || animal === 'cat') {
+      let newAnimal = new Animal(name, animal);
+      let current = this.head;
 
-    let current = this.head;
-
-    if (!this.head) {
-      this.head = newAnimal;
-    } else {
-
-      while (current.next) {
-        current = current.next;
+      if (!this.head) {
+        this.head = newAnimal;
+      } else {
+        while (current.next) {
+          current = current.next;
+        }
+        current.next = newAnimal;
       }
-
-      current.next = newAnimal;
+      return this;
+    } else {
+      return 'Only dogs and cats are allowed';
     }
   }
 
   dequeue(pref = null) {
+
     let current = this.head;
 
     if (pref === 'dog') {
+      if (this.head.type === 'dog') {
+        this.head = this.head.next;
+        current.next = null;
+        return current;
+      } else {
+        while (current) {
+          if (current.next.type === 'dog') {
+            let adopted = current.next;
 
-      while (current) {
-        if (current.type === 'dog') {
+            current.next = current.next.next;
 
-          if (current === this.head) {
-            this.head = this.head.next;
-            return current;
+            adopted.next = null;
+
+            return adopted;
           }
-
-
-
-
-
-          return current
+          current = current.next;
         }
       }
-
     }
 
 
     if (pref === 'cat') {
+      if (this.head.type === 'cat') {
+        this.head = this.head.next;
+        current.next = null;
+        return current;
+      } else {
+        while (current) {
+          if (current.next.type === 'cat') {
+            let adopted = current.next;
 
-      while (current) {
-        if (current.type === 'cat') {
+            current.next = current.next.next;
 
-          if (current === this.head) {
-            this.head = this.head.next;
-            return current;
+            adopted.next = null;
+
+            return adopted;
           }
-
-
-
+          current = current.next;
         }
       }
-
     }
+
+
+    this.head = current.next;
+    current.next = null;
+    return current;
   }
+
 }
 
 module.exports = AnimalShelter;
